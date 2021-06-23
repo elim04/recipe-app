f435948fb696b6831597efa79adeb4cfdad572c0
Emily <emily.elim04@gmail.com>
2021-06-23 15:53:58 -0400

    add basic card styling to recipes

--- src/components/RecipeListItem.tsx
+++ src/components/RecipeListItem.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core";
+import { Card, CardActions, CardContent } from "@material-ui/core";
+
 interface RecipeListItemProps {
   recipeName: string;
   servingSize: number;
 }
 
+const useStyles = makeStyles({
+  root: {
+    width: 275,
+    margin: 10,
+    backgroundColor: "rgb(221 227 255 / 87%)",
+  },
+});
+
 const RecipeListItem: React.FC<RecipeListItemProps> = ({
   recipeName,
   servingSize,
 }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h3>{recipeName}</h3>
-      <p>Serving Size: {servingSize}</p>
-    </div>
+    <Card className={classes.root}>
+      <CardContent>
+        <h3>{recipeName}</h3>
+        <p>Serving Size: {servingSize}</p>
+      </CardContent>
+    </Card>
   );
 };
 
