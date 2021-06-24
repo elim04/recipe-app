aec04acdb15d15f919ced2b70177fe49b031b449
Emily <emily.elim04@gmail.com>
2021-06-24 11:02:29 -0400

    add scard styling to map return

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import RecipeListItem from "./RecipeListItem";
 import { Recipe } from "../recipe.model";
+import { makeStyles } from "@material-ui/core";
+import { Card, CardContent } from "@material-ui/core";
 
 //describes props for this component
 interface RecipeListProps {
@@ -8,16 +10,30 @@ interface RecipeListProps {
   onAddRecipe: (recipe: Recipe) => void;
 }
 
+const useStyles = makeStyles({
+  root: {
+    width: 275,
+    margin: 10,
+    backgroundColor: "rgb(221 227 255 / 87%)",
+  },
+});
+
 const RecipeList: React.FC<RecipeListProps> = (props) => {
+  const classes = useStyles();
+
   return (
     <div>
       {props.recipesData.map((recipeItem) => {
         return (
-          <RecipeListItem
-            key={recipeItem.id}
-            recipeName={recipeItem.name}
-            servingSize={recipeItem.servingSize}
-          />
+          <Card className={classes.root}>
+            <CardContent>
+              <RecipeListItem
+                key={recipeItem.id}
+                recipeName={recipeItem.name}
+                servingSize={recipeItem.servingSize}
+              />
+            </CardContent>
+          </Card>
         );
       })}
     </div>
