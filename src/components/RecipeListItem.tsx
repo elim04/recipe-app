2967c69db8cc3339cd9d8a8df27cad437013c5ae
Emily <emily.elim04@gmail.com>
2021-06-24 11:02:42 -0400

    remove individual card styling in single component

--- src/components/RecipeListItem.tsx
+++ src/components/RecipeListItem.tsx
@@ -1,33 +1,18 @@
 import React from "react";
-
-import { makeStyles } from "@material-ui/core";
-import { Card, CardContent } from "@material-ui/core";
-
 interface RecipeListItemProps {
   recipeName: string;
   servingSize: number;
 }
 
-const useStyles = makeStyles({
-  root: {
-    width: 275,
-    margin: 10,
-    backgroundColor: "rgb(221 227 255 / 87%)",
-  },
-});
-
 const RecipeListItem: React.FC<RecipeListItemProps> = ({
   recipeName,
   servingSize,
 }) => {
-  const classes = useStyles();
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <h3>{recipeName}</h3>
-        <p>Serving Size: {servingSize}</p>
-      </CardContent>
-    </Card>
+    <div>
+      <h3>{recipeName}</h3>
+      <p>Serving Size: {servingSize}</p>
+    </div>
   );
 };
 
