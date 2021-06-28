e1fdd9bb4e04fae2c7b2b4c5a2040ba69afcdb07
Emily <emily.elim04@gmail.com>
2021-06-28 13:36:51 -0400

    add recipe list type to prop types

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import RecipeList from "./RecipeList";
 import NewRecipe from "./NewRecipe";
 import { Recipe } from "../recipe.model";
+import { RecipeListObj } from "../recipeList.model";
 
 import "./App.css";
 
@@ -12,18 +13,21 @@ import "./App.css";
 import { data } from "../testdata";
 
 const App: React.FC = () => {
-  const [recipes, setRecipes] = useState<any>(data);
-  console.log(recipes);
+  const [recipes, setRecipes] = useState<RecipeListObj>(data);
 
   const onAddRecipe = (recipe: Recipe) => {
-    // let recipeKey = Math.floor(Math.random()*100);
-    // setRecipes((prevRecipes) => {...prevRecipes, prevRecipes[recipeKey] = recipe});
+    let recipeKey = Math.floor(Math.random() * 100);
+    setRecipes((prevRecipes: RecipeListObj) => {
+      return {
+        ...prevRecipes,
+        recipeKey: recipe,
+      };
+    });
   };
 
   const deleteRecipe = (recipeID: number) => {
-    // setRecipes((prevRecipes: Recipe[]) => {
-    //   return prevRecipes.filter((recipe) => recipe.id !== recipeID);
-    // });
+    let { [recipeID]: omit, ...res } = recipes;
+    setRecipes(res);
   };
 
   return (
