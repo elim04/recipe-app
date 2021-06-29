c91dfa59532f76257296fbca42480b6fd6986069
Emily <emily.elim04@gmail.com>
2021-06-29 11:44:10 -0400

    create instructions component

---
+++ src/components/RecipeCard/InstructionsSection.tsx
@@ -0,0 +1,11 @@
+interface InstructionsProps {
+  instructionsData: string[];
+}
+
+const InstructionsSection: React.FC<InstructionsProps> = ({
+  instructionsData,
+}) => {
+  return <div>Instructions</div>;
+};
+
+export default InstructionsSection;
