d3d262789fec9c08b9a5f13566a06ba3eebd430b
Emily <emily.elim04@gmail.com>
2021-06-29 11:43:43 -0400

    create cookware component

---
+++ src/components/RecipeCard/CookwareSection.tsx
@@ -0,0 +1,9 @@
+interface CookwareProps {
+  cookwareData: string[];
+}
+
+const CookwareSection: React.FC<CookwareProps> = ({ cookwareData }) => {
+  return <div>Cookware</div>;
+};
+
+export default CookwareSection;
