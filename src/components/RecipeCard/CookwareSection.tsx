8eb87bc73ea8a30dbfeac8e74c57cdf9af06d80c
Emily <emily.elim04@gmail.com>
2021-06-29 15:35:06 -0400

    adding cookware data

--- src/components/RecipeCard/CookwareSection.tsx
+++ src/components/RecipeCard/CookwareSection.tsx
@@ -1,9 +1,19 @@
+import { List, ListItem } from "@material-ui/core";
+
 interface CookwareProps {
   cookwareData: string[];
 }
 
 const CookwareSection: React.FC<CookwareProps> = ({ cookwareData }) => {
-  return <div>Cookware</div>;
+  const cookwareRows = cookwareData.map((item) => {
+    return <ListItem key={item}>{item}</ListItem>;
+  });
+  return (
+    <div>
+      <h2>Cookware Required</h2>
+      <List className="cookware-list">{cookwareRows}</List>
+    </div>
+  );
 };
 
 export default CookwareSection;
