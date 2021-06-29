c00ada7897d20186c7fe25f7844a42bbc3eca9f8
Emily <emily.elim04@gmail.com>
2021-06-29 15:35:23 -0400

    adding instructions data

--- src/components/RecipeCard/InstructionsSection.tsx
+++ src/components/RecipeCard/InstructionsSection.tsx
@@ -1,3 +1,4 @@
+import { List, ListItem } from "@material-ui/core";
 interface InstructionsProps {
   instructionsData: string[];
 }
@@ -5,7 +6,17 @@ interface InstructionsProps {
 const InstructionsSection: React.FC<InstructionsProps> = ({
   instructionsData,
 }) => {
-  return <div>Instructions</div>;
+  const instructionsRows = instructionsData.map((item) => {
+    let count = 1;
+    return <ListItem key={count + 1}>{item}</ListItem>;
+  });
+
+  return (
+    <div>
+      <h2>Instructions</h2>
+      <List>{instructionsRows}</List>
+    </div>
+  );
 };
 
 export default InstructionsSection;
