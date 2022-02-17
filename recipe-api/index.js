8bfa3353a5a35b9cf1f41964d5c8bc059758d819
Emily <emily.elim04@gmail.com>
2022-02-17 17:07:06 -0500

    add router object

--- recipe-api/index.js
+++ recipe-api/index.js
@@ -3,11 +3,44 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use("/", router);
+router.all((req, res) => {
   res.send("Hello, world!");
 });
+
+function handler(req, res, next) {
+  const { username } = req.body;
+
+  if (!username) {
+    next(new Error("missing username"));
+    return;
+  }
+
+  res.json({ message: `Hello ${username}!` });
+}
+
+router.post("/welcome", handler);
+
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.toString() });
+});
+
+// app.post("/", (req, res) => {
+//   res.send("Cool API request");
+// });
+
+// Router object
+// A router object is an isolated instance of middleware and routes
+
+router.get("/ingredients", (req, res, next) => {
+  res.send("You are in the ingredients path");
+});
+
+app.use("/recipes", router);
 
 const PORT = process.env.PORT || 8080;
 
